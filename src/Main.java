1912127ede007f80ac5ae9e1e7f6be9e7619fc76
AlexandrMakeev <pipifax@bk.ru>
2022-07-08 15:01:06 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int replenishmentAmount = 2000;//The amount by which the account is replenished
-        int accountBalance = 0;//The amount in the account
-        int balanceAfterReplenishment = replenishmentAmount + accountBalance;//The amount in the account after replenishment
-        int bonuses = replenishmentAmount / 100;//The amount bonuses
+        double replenishmentAmount = 1000.5;//The amount by which the account is replenished
+        double accountBalance = 0;//The amount in the account
+        double balanceAfterReplenishment = replenishmentAmount + accountBalance;//The amount in the account after replenishment
+        int bonuses = (int)replenishmentAmount / 100;//The amount bonuses
         if (replenishmentAmount > 1000) {
             System.out.println(balanceAfterReplenishment + bonuses);
         } else {
